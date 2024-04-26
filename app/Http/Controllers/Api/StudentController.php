13b6c4caa806fd20e5291ddb03c9226ee5c9942b
rabbaniST <golamrabbanig5@gmail.com>
2024-04-26 17:41:19 +0600

    laravel crud rest-api, with index and store methods

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $student = Student::all();
+        if ($student->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'student' => $student,
+            ], 200);
+        }
+        return response()->json([
+            'status' => 404,
+            'message' => 'No Records Found',
+        ], 404);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:200',
+            'course' => 'required|string|max:200',
+            'email' => 'required|email|max:100',
+            'phone' => 'required|digits:10'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+            if ($student) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successful"
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something Went Wrong"
+                ], 500);
+            }
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return "hello Words";
+})->middleware('auth:sanctum');
+
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::Post('/students', [StudentController::class, 'store']);
